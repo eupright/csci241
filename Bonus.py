cf1a09796b5ba7bf85da6060e09ccc9a995a52ad
eupright <98702086+eupright@users.noreply.github.com>
2022-11-10 09:44:42 -0500

    Delete Bonus.py

--- Bonus.py
+++
@@ -1,8 +0,0 @@
-import random
-import copy
-import sys
-import trace
-from Sort import *
-
-tracer = trace.Trace(trace = 1, count = 0, outfile = 'Desktop/CSCI140/Sort.cover')
-tracer.run('insertion_sort([0,5,4,7])')
